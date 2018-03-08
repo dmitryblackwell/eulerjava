e3b3d2fa83d659f4e08f867dc4136e85d26fd5e5
dmitryblackwell <dmitryblackwell@gmail.com>
2018-03-08 02:40:23 +0200

    Problem 18,20 solved
    Solved 18th and 20th problem. I skipp 19th problem, come back to it later. Also I deleted binary tree package, because there are no need in it anymore.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main { // мой код - мои комменты
     public static void main(String[] args) {
         ConsoleInterface ci=new ConsoleInterface();
         boolean isExit = false;
-        ci.runProblem(18);
+        ci.runProblem(20);
         while (isExit){
             System.out.print("cmd: ");
             Scanner in = new Scanner(System.in); // ты пидор
