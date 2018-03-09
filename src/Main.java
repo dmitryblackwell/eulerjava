cbde7bebd92c250e39ee6394bc45634dee80c2cb
dmitryblackwell <dmitryblackwell@gmail.com>
2018-03-09 20:00:32 +0200

    Problem 19 solved
    Solve 19th problem. I was not paying attention and because of it made a lot of mistake. Have to be more careful.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main { // мой код - мои комменты
     public static void main(String[] args) {
         ConsoleInterface ci=new ConsoleInterface();
         boolean isExit = false;
-        ci.runProblem(21);
+        ci.runProblem(19);
         while (isExit){
             System.out.print("cmd: ");
             Scanner in = new Scanner(System.in); // ты пидор
--- src/com/blackwell/level_01/Problem_019.java
+++ src/com/blackwell/level_01/Problem_019.java
@@ -1,4 +1,71 @@
 package com.blackwell.level_01;
 
 public class Problem_019 {
+    class Date{
+        private int day;
+        private String WeekDay;
+        private int month;
+        private int year;
+
+        private int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        private String[] WeekDaysName={"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        public Date(int day, String weekDay, int month, int year) {
+            this.day = day;
+            WeekDay = weekDay;
+            this.month = month;
+            this.year = year;
+        }
+        private int getWeekDayNumber(){
+            for(int i=0; i<7; ++i)
+                if (WeekDay.equals(WeekDaysName[i]))
+                    return i;
+            return 0;
+        }
+        public int getDay() {
+            return day;
+        }
+
+        public String getWeekDay() {
+            return WeekDay;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        private boolean isLeap(){
+            return  ((year%4 == 0 && year%100 != 0) || year%400 == 0);
+        }
+        public void plus(int days){
+            day += days;
+            int WeekDayNumber = (getWeekDayNumber()+days)%7;
+            WeekDay = WeekDaysName[WeekDayNumber];
+            while (day > daysInMonth[month-1]) {
+                day -= daysInMonth[month-1];
+                month ++;
+                if(month == 13) {
+                    month = 1;
+                    year++;
+                    daysInMonth[1] = isLeap() ? 29 : 28;
+
+                }
+            }
+        }
+        public void print(){
+            System.out.println(day+"."+month+"."+year+" "+WeekDay);
+        }
+    }
+
+    public Problem_019() {
+        Date date=new Date(6,"Sunday",1,1901);
+        int sundays = 0;
+
+        while(date.getYear()< 2001){;
+            date.plus(7);
+            if (date.getWeekDay().equals("Sunday") && date.getDay() == 1)
+                sundays ++;
+        }
+        System.out.println(sundays);
+    }
 }
