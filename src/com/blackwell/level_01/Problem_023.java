1ea21dc65d36f954a56ad76af2ef2fb6a79641b1
dmitryblackwell <dmitryblackwell@gmail.com>
2018-03-10 12:44:54 +0200

    Problem 23 Beta
    My solution for problem 23 doesnt work, but I dont know why. So I commit my changes, delete whole code and start from the scrach

--- src/com/blackwell/level_01/Problem_023.java
+++ src/com/blackwell/level_01/Problem_023.java
@@ -1,8 +1,11 @@
 package com.blackwell.level_01;
 
+import java.util.ArrayList;
+
 public class Problem_023 {
     private static final int GREATEST_NUM = 28123;
-
+    private boolean[] isAbundant = new boolean[GREATEST_NUM];
+    private boolean[] isSumOfTwoAbundants = new boolean[GREATEST_NUM+1];
     private int getSumDivisors(int n){
         int sum=1;
         for(int i=2; i<Math.sqrt(n); ++i)
@@ -12,22 +15,39 @@ public class Problem_023 {
     }
 
     private boolean isAbundant(int n){
-        return getSumDivisors(n)>n;
+        return n > 11 && getSumDivisors(n)>n;
+    }
+    private void AbundantInit(){
+        for(int i=1; i < GREATEST_NUM; ++i)
+            isAbundant[i] = isAbundant(i);
+    }
+    private void SumOfTwoAbundantsInit(){
+        for(int i=0; i<isSumOfTwoAbundants.length; ++i)
+            isSumOfTwoAbundants[i] = false;
+
+        for(int i=0; i<isSumOfTwoAbundants.length; ++i){
+            for(int j=i; j<isSumOfTwoAbundants.length; ++j){
+                if( i+j < GREATEST_NUM && isAbundant[i] && isAbundant[j] )
+                    isSumOfTwoAbundants[i+j] = true;
+            }
+        }
     }
 
     private boolean canBeWrittenAsSum(int n){
-        for(int i=0; i<n; ++i){
-            if (isAbundant(i) && isAbundant(n-i))
+        for(int i=1; i<=n; ++i)
+            if (isAbundant[i] && isAbundant[n-i])
                 return true;
-        }
         return false;
     }
 
     public Problem_023() {
+        AbundantInit();
+        SumOfTwoAbundantsInit();
         int sum=0;
-        for(int i=0; i < GREATEST_NUM; ++i) {
-            if (!canBeWrittenAsSum(i)) {
+        for(int i=1; i <= GREATEST_NUM; ++i) {
+            if (isSumOfTwoAbundants[i]) {
                 sum += i;
+                //System.out.println(i);
             }
         }
         System.out.println(sum);
